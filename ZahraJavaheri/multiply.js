6a1606e15c0eb7ccfbc5803ef78c8e2ccd4b83c6
zahra javaheri <82215570+zahrajvh@users.noreply.github.com>
2022-10-11 15:41:11 +0330

    Create multiply.js

---
+++ ZahraJavaheri/multiply.js
@@ -0,0 +1,9 @@
+var real1 = prompt("قسمت حقیقی عدد اول را وارد کنید");
+var imaginary1 = prompt("قسمت موهومی عدد اول را وارد کنید");
+var real2 = prompt("قسمت حقیقی عدد دوم را وارد کنید");
+var imaginary2 = prompt("قسمت موهومی عدد دوم را وارد کنید");
+
+var real_answer = Number(real1) * Number(real2) - Number(imaginary1) * Number(imaginary2);
+var imaginary_answer = Number(real1) * Number(imaginary2) + Number(imaginary1) * Number(real2);
+var answer = real_answer + "+" + imaginary_answer + "i";
+alert(answer);
