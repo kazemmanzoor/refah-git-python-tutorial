7aa150e4dc0adbd43548429d2366d978a6d86d43
Zahra Fatemi <114424595+Zfatemi24@users.noreply.github.com>
2022-10-17 16:35:29 +0330

    Update three.py

--- Zahra Fateemi/ex1/ex2/three.py
+++ Zahra Fateemi/ex1/ex2/three.py
@@ -5,11 +5,4 @@ def factorial(n):
         return n * factorial(n-1)
 number = int(input(" Enter number: "))
 result = factorial(number)
-print(result)def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
-number = int(input(" Enter number: "))
-result = factorial(number)
 print(result)
