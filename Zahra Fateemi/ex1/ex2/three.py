950d63983dace6de54033c4ec0ea48844c4dfdde
Zahra Fatemi <114424595+Zfatemi24@users.noreply.github.com>
2022-10-17 12:25:43 +0330

    Create three.py

---
+++ Zahra Fateemi/ex1/ex2/three.py
@@ -0,0 +1,15 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+number = int(input(" Enter number: "))
+result = factorial(number)
+print(result)def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+number = int(input(" Enter number: "))
+result = factorial(number)
+print(result)
