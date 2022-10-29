c158a5219a6dec44b326a8d753b5d40996f76c91
mahsamehrabi <114923339+mahsamehrabi@users.noreply.github.com>
2022-10-29 17:03:52 +0330

    Create 3.py

---
+++ Mahsa Hajimehrabi/home work3/3.py
@@ -0,0 +1,15 @@
+import InputListModule as Rec
+
+list1 = []
+list2 = []
+Rec.input5(list1)
+Rec.input5(list2)
+
+list1.extend(list2)
+print(list1)
+
+for i in range(len(list1)):
+    for j in range(i + 1, len(list1)):
+        if list1[i] > list1[j]:
+           list1[i], list1[j] = list1[j], list1[i]
+print (list1)
