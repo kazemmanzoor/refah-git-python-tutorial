894e538d8ec1b799bdce15b71ce92ed057b9fbe4
salehe abbasi <88197001+befor921after@users.noreply.github.com>
2022-11-02 20:38:17 +0330

    Delete Salehe Abbasi directory

--- Salehe Abbasi/func-practice/third.py
+++
@@ -1,7 +0,0 @@
-def factorail(n):
-    if n==0:
-        return 1
-    else:
-        return n*factorail(n-1)
-
-print(factorail(3))
--- Salehe Abbasi/home work4/practice5.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-plt.plot([
-        "Ghom","Tehran","Fars","kerman","zanjan","kerman"
-    ],[1373000, 13807000,5006000, 3299000,1095000,3299000])
-plt.ylabel('population')
-plt.xlabel("city")
-plt.show()
