7ab620afa30dc8404b4dfe00877fbe7a1183a9fe
salehe abbasi <88197001+befor921after@users.noreply.github.com>
2022-11-02 20:21:57 +0330

    Add files via upload

---
+++ Salehe Abbasi/home work4/practice2.py
@@ -0,0 +1,9 @@
+def myFunc(mytxt):
+    txt = mytxt.upper()
+    counter = 0
+    counter = txt.count("A")+txt.count("E")+txt.count("O")+txt.count("I")+txt.count("U")
+    return counter
+
+
+#---------------------------------------------------------------------------
+print(myFunc("i am salehe abbasi"))
