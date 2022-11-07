344d9ae6fecef4abdb74a8760f5bbf7445bb77d7
Zahra Fatemi <114424595+Zfatemi24@users.noreply.github.com>
2022-11-08 00:25:24 +0330

    Create three.py

---
+++ Zahra Fateemi/ex1/ex4/three.py
@@ -0,0 +1,8 @@
+input_str = input("enter  string:  ")
+input_char =input("enter character:")
+count = 0
+for i in input_str :
+    if i == input_char :
+        count=count+1
+    
+print (str(count))
