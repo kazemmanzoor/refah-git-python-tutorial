63baa8918fccaa4fc037773acc2031be2ba02995
JavidMansooreh <114851533+MimJavid@users.noreply.github.com>
2022-11-11 15:22:11 +0330

    Add files via upload

---
+++ javid/exam1week2.py
@@ -0,0 +1,12 @@
+#number1=input("please inter your num:")
+#number2=input("please inter your num:")
+def mazrab(number1,number2):
+    if number1%number2==0:
+        return True
+    else :
+        return False
+print(mazrab(14,7))
+        
+
+
+
