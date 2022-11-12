a1931924bac6f9ff6afd2bcc964b507f2112ec8c
JavidMansooreh <114851533+MimJavid@users.noreply.github.com>
2022-11-12 19:41:49 +0330

    Update exam1week2.py

--- javid/exam1week2.py
+++ javid/exam1week2.py
@@ -1,12 +1,11 @@
-#number1=input("please inter your num:")
-#number2=input("please inter your num:")
-def mazrab(number1,number2):
-    if number1%number2==0:
+def mazrab(num1,num2):
+    if num1%num2==0:
         return True
     else :
         return False
-print(mazrab(14,7))
-        
+num1=float(input("please inter your num:"))
+num2=float(input("please inter your num:"))
+print(mazrab(num1,num2))
 
 
 
