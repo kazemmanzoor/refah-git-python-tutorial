94c1205ad7d4c00a228152f250f850f0762ed23f
ftmyoOou <114109152+ftmyoOou@users.noreply.github.com>
2022-11-21 22:42:30 +0330

    Update 1.py

--- Fatemeh Younesi/week3/1.py
+++ Fatemeh Younesi/week3/1.py
@@ -1,1 +1,9 @@
-
+def convert(a,b):
+    for i in range(len(a)):
+        if b.count(a[i])>0:
+            continue
+        else:
+            b.append(a[i])
+a=input("add list a=")
+b=input("add list b=")
+print(b)
