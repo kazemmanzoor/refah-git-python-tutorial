a1aa3194ac23387274c18fa484f22907b4a7fe5d
Zahra <114489797+ZRamin@users.noreply.github.com>
2022-11-22 22:39:26 +0330

    Update tamrin1.py

--- Zahra Ramin/week3/tamrin1.py
+++ Zahra Ramin/week3/tamrin1.py
@@ -1,1 +1,9 @@
-print("hello!!")
+list1=[123,456]
+list2=[789,123]
+for i in range(len(list1)):
+    if(list2.count(list1[i]))>0:
+        continue
+    else:
+        list2.append(list1[i])
+
+print(list2)
