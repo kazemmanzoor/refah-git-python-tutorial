2178b908946bb97663b3014a4c2795c9dea5002a
zahra sadte yaghoobi <80329525+zsyhd@users.noreply.github.com>
2022-11-22 15:35:27 +0330

    Create 1.py

---
+++ Zahra sadte yaghoobi/tamrin 5/1.py
@@ -0,0 +1,10 @@
+def func(u):
+    sum = []
+    for x in u:
+        count = 0
+        for i in x:
+            count = count+i
+        Sum.append(count)
+    return Sum
+list1 = [[77,5,88],[21,50,9],[33,55,10]]
+print(Func(list1))
