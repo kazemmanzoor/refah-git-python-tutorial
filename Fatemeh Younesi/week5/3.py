eb7151901d0b28335389cb699851d7d7a36d8e38
ftmyoOou <114109152+ftmyoOou@users.noreply.github.com>
2022-11-23 01:07:57 +0330

    Update 3.py

--- Fatemeh Younesi/week5/3.py
+++ Fatemeh Younesi/week5/3.py
@@ -1,1 +1,10 @@
-
+a=[[2,3,4],
+   [3,4,5]]
+b=[[4,-3,12],
+   [1,1,5],
+   [1,3,2]]
+c=[]
+for person in a and b:
+    for i in range(0,(len(a) and len(b))):
+        c=(a[i])*(b[i])
+        c=c+c
