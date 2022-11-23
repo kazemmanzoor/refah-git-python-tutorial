58baf5b6f598ba11cddbd14dc23bb07ad69cc5a2
0023413247 <118829766+0023413247@users.noreply.github.com>
2022-11-23 15:30:02 +0330

    Add files via upload

---
+++ test 1.py
@@ -0,0 +1,17 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=== RESTART: C:/Users/user/AppData/Local/Programs/Python/Python310/test v1.py ==
+insert number4
+insert number5
+4
+4
+44
+48
+444
+492
+4444
+4936
+44444
+49380
+None
---
+++ test v2.py
@@ -0,0 +1,9 @@
+#def n_letter_dictonary(word)
+sr=" i am student in refah university"
+myarr=[]
+myarr=sr.split(" ")
+print(myarr)
+ans={}
+for item in myarr:
+    print (item,len(item))
+    
