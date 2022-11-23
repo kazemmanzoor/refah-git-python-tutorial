d0107ba1d53a126a8b170e9a22ca2ad0d09c4b1d
mahsamehrabi <114923339+mahsamehrabi@users.noreply.github.com>
2022-11-23 15:52:45 +0330

    Update 2.py

--- Mahsa Hajimehrabi/exam/2.py
+++ Mahsa Hajimehrabi/exam/2.py
@@ -1,1 +1,18 @@
+def n_letter_dictionary(str1):
+    Mylist=str1.split()
+    print(Mylist)
+    answer={}
+    for item in Mylist:
+        val1=answer.get(len(item),[])
+        if item in val1:
+            continue
+        else :
+            val2=answer.get(len(item),[] )
+            val2.append(item)
+            answer[len(item)]=val2
+            print(n_letter_dictionary(val1))
+    return answer
 
+
+val="the way you see people is the way you treat them and the way you treat them is what they become"
+n_letter_dictionary(val)
