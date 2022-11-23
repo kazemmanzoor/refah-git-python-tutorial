d77cd334880a7c5bbfddff126e1edda978df6e8d
Ftmh Behradmehr <114109458+ftmhbehradmehr@users.noreply.github.com>
2022-11-23 15:18:46 +0330

    Create q2.py

---
+++ Ftmh Behradmehr/midterm/q2.py
@@ -0,0 +1,10 @@
+a="I am student in refah university in tehran"
+def n_letter_dictionary(str):
+    mylist=str.split("")
+    mydic={}
+    for item in mylist:
+        mydic[len(item)]
+    print(mylist)
+
+#print(n_letter_dictionary(a))
+    
