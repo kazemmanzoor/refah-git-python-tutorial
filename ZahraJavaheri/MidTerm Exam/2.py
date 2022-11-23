3298cb061db2d1cc56320ae42ebd115f74219d34
zahra javaheri <82215570+zahrajvh@users.noreply.github.com>
2022-11-23 15:05:21 +0330

    Create 2.py

---
+++ ZahraJavaheri/MidTerm Exam/2.py
@@ -0,0 +1,25 @@
+def n_letter_dictionary(str):
+    
+    str = str.lower()
+    WordList = str.split(" ")
+
+    lenlist = []
+    FinalDict = {}
+
+    for i in WordList:
+        lenlist.append(len(i))
+    count = max(lenlist)
+
+    for z in range(count):
+        mylist = []
+        for Word in WordList:
+            
+            if(len(Word) == z+1 and (Word in mylist) == False):
+                mylist.append(Word)
+        if(len(mylist) != 0):
+            FinalDict[z+1] = mylist
+    print(FinalDict)
+    
+
+string = input("Enter the text:")
+n_letter_dictionary(string)
