9ff69cbe21df456cce89ef254d7801070a01e483
Fateme Mardani | Yamin <79452992+fmrdni@users.noreply.github.com>
2022-11-23 05:36:17 +0330

    Update 04_WordReverser.py

--- Fateme Mardani/HomeWorks/04/04_WordReverser.py
+++ Fateme Mardani/HomeWorks/04/04_WordReverser.py
@@ -1,1 +1,12 @@
 #temp
+def reverser(string):
+   w = ''
+   splited = string.split(' ')
+   for i in splited:
+     rvs = list(i+' ')
+     rvs.reverse()
+     for j in rvs:
+       w = w + j
+   return w
+input_string = input('your string:\t')
+print(reverser(input_string))
