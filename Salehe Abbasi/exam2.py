badb1437548925a0972276e09225bbf6387c690b
salehe abbasi <88197001+befor921after@users.noreply.github.com>
2022-11-23 15:01:27 +0330

    Chabge3

--- Salehe Abbasi/exam2.py
+++ Salehe Abbasi/exam2.py
@@ -3,7 +3,11 @@ def n_letter_dictionary(str1):
     dic1 = {}
     dic2 = {}
     for item in list1:
-        dic1[len(item)] =dic1.get(len(item) , "") + " "+ item
+        val3 = dic1.get(len(item) , "").split(" ")
+        if item in val3:
+            continue
+        else:
+            dic1[len(item)] =dic1.get(len(item) , "") + " "+ item
     for item in list1:
         val2 = dic1.get(len(item) ).split(" ")
         dic2[len(item)] = val2[1::]
@@ -11,5 +15,5 @@ def n_letter_dictionary(str1):
 
 
 #------------------------------
-val1 = "i am student in refah"
+val1 = "i am am student in refah"
 print(n_letter_dictionary(val1))
