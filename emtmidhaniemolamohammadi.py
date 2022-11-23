0c9c768b9a18b3a9a6907735ae7e270c0e5d6632
HeyHancpu <114977130+HeyHancpu@users.noreply.github.com>
2022-11-23 15:25:01 +0330

    Add files via upload

---
+++ emtmidhaniemolamohammadi.py
@@ -0,0 +1,31 @@
+
+
+def n_letter_dictionary(str):
+    listsword = str.split("")
+    dict6={}
+    
+for n in wordslist:
+    len(n)
+    dict6 [len(n)] = dict6.get(len(n),"")+""+n
+    print(dict6)
+    
+ return dict6;
+
+val6= "The way you see people is the way you treat them and the way you treat them is what they become"
+print(n_letter_dictionary(val6))
+
+    
+        
+        
+        
+    
+
+
+     
+
+
+
+    
+    #haniemolamohammadi soal 2
+
+
