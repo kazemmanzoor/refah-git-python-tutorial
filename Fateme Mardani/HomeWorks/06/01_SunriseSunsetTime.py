5e8198a7fe00b9300eeb93221de5117cbe345e0e
Fateme Mardani | Yamin <79452992+fmrdni@users.noreply.github.com>
2022-12-13 20:16:25 +0330

    Update 01_SunriseSunsetTime.py

--- Fateme Mardani/HomeWorks/06/01_SunriseSunsetTime.py
+++ Fateme Mardani/HomeWorks/06/01_SunriseSunsetTime.py
@@ -1,1 +1,14 @@
-#
+# Mashhad Sunrise & Sunset Time
+import datetime
+import requests
+import json
+
+def fanc(lat , lng):
+    req = "https://api.sunrise-sunset.org/json?lat="+str(lat)+"&lng="+str(lng)
+    resApi = req.get(req);
+
+    return resApi.text
+  
+  
+output = json.loads( fanc(36.305729, 59.57837)).get("results")
+print("Sunrise: ", ans.get("sunrise") , "\nSunset: ",output.get("sunset"))
