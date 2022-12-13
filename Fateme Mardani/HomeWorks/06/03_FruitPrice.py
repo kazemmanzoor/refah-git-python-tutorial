e0fcac40c08e1a5b26f55c7e8e93a1f24fcabb6e
Fateme Mardani | Yamin <79452992+fmrdni@users.noreply.github.com>
2022-12-13 19:54:31 +0330

    Create 03_FruitPrice.py

---
+++ Fateme Mardani/HomeWorks/06/03_FruitPrice.py
@@ -0,0 +1,5 @@
+import numpy as np
+kg = np.array([[4, 3, 2], [-2, 2, 3], [3, -5, 2]])
+income = np.array([2, -10, -4])
+fruitPrice = np.linalg.solve(a, b)
+print ("gheymate har kilo porteghal va narengi be tartib: ",x)
