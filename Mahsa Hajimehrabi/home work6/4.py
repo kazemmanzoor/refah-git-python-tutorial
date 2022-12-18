723c68717d31abd4f6022f0dec662632c908838d
mahsamehrabi <114923339+mahsamehrabi@users.noreply.github.com>
2022-12-18 10:39:02 +0330

    Create 4.py

---
+++ Mahsa Hajimehrabi/home work6/4.py
@@ -0,0 +1,8 @@
+import numpy as np
+ np.array([
+             [4 , 3 , 2],
+             [-2 , 2 ,3],
+             [3,-5 ,2]
+                    ])
+ np.array([25 , -10 ,-4])
+print("X = ",result[0],"\nY = ",result[1],"\nZ = ",result[2] )
