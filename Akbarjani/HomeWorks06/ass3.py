964ab96f200556faf48d09d57345fea0d94e13fc
Akbarjani <Akbarjanimoh@gmail.com>
2022-12-19 22:08:09 +0330

    HW06

---
+++ Akbarjani/HomeWorks06/ass3.py
@@ -0,0 +1,5 @@
+import numpy as np
+a = np.array([[20,10],[17,22]])
+b = np.array([350,500])
+x = np.linalg.solve(a,b)
+print(x)
---
+++ Akbarjani/HomeWorks06/ass4.py
@@ -0,0 +1,5 @@
+import numpy as np
+a = np.array([[4,3,2],[-2,2,3],[3,-5,2]])
+b = np.array([25,-10,-4])
+x = np.linalg.solve(a,b)
+print(x)
