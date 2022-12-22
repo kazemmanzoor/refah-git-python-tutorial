cc1921f6669971487a247ef9c13b9bf095204501
Zahra Fatemi <114424595+Zfatemi24@users.noreply.github.com>
2022-12-23 00:22:07 +0330

    Update three.py

--- Zahra Fateemi/ex1/ex6/three.py
+++ Zahra Fateemi/ex1/ex6/three.py
@@ -1,10 +1,7 @@
 import numpy as np
-
 A = np.array([
              [20 , 10],
-             [17 , 22]
-                    ])
-
+             [17 , 22]])
 B = np.array([350 , 500])
 result = np.linalg.solve(A , B)
 print("orange = ",result[0],"\nnarengy = ",result[1])
