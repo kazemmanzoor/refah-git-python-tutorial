cefdcf22e9bfd7bc967282176db89cc90c7f5a8e
befor921after <hendrix291lincoln@gmail.com>
2023-01-23 13:26:43 +0330

    second commit

---
+++ Fateme Hosseini/sixth practice/1.py
@@ -0,0 +1,14 @@
+import requests
+import json
+def getSunSet(lat , lng):
+    reqSentece = "https://api.sunrise-sunset.org/json?lat="+str(lat)+"&lng="+str(lng)
+    responseApi = requests.get(reqSentece);
+
+    return responseApi.text
+    
+
+#------------------------------------------------------
+
+ans =json.loads( getSunSet(36.305729, 59.57837)).get("results")
+
+print("sunrise: ", ans.get("sunrise") , "\nsunset: ",ans.get("sunset"))
